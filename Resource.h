dd6fced702d980af39049c1021c1249700be64f9
atomictraveller <125493303+atomictraveller@users.noreply.github.com>
2023-03-06 17:16:03 -0700

    Add files via upload

---
+++ Resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by lofihifi.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_LOFIHIFI_DIALOG	102
+#define IDD_ABOUTBOX			104
+#define IDM_ABOUT				105
+#define IDM_OPEN				106
+#define IDM_SAVE				107
+#define IDM_REMOVELOOPS			108
+#define IDM_NEWDATA				109
+#define IDM_OPENDATA			110
+#define IDM_SAVEDATA			111
+#define IDM_TRAIN				112
+#define IDM_APPLYHIFI			113
+#define IDM_EXIT				120
+#define IDI_LOFIHIFI			121
+#define IDI_SMALL				122
+#define IDC_LOFIHIFI			123
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
